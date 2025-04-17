dba89694add1229a8e3f40a14963a4e96d09ac88
tcheng-altarum <thanh.cheng@altarum.org>
2025-04-17 01:58:45 +0000

    commit

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,10 +1,41 @@
 from djongo import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class User(models.Model):
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
     _id = models.ObjectIdField()
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
 
 class Team(models.Model):
     _id = models.ObjectIdField()
