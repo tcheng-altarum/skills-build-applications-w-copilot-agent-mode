c2be1e3eb94b9b4ec07a0141451bd897fcc50dc1
tcheng-altarum <thanh.cheng@altarum.org>
2025-04-17 02:36:47 +0000

    commit step 4

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,12 +6,13 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
+    base_url = "https://turbo-waddle-77vqwqgj56x2wgw-8000.app.github.dev" if request.get_host().startswith("turbo-waddle") else "http://localhost:8000"
     return Response({
-        'users': 'api/users/',
-        'teams': 'api/teams/',
-        'activities': 'api/activities/',
-        'leaderboard': 'api/leaderboard/',
-        'workouts': 'api/workouts/'
+        'users': f'{base_url}/api/users/',
+        'teams': f'{base_url}/api/teams/',
+        'activities': f'{base_url}/api/activities/',
+        'leaderboard': f'{base_url}/api/leaderboard/',
+        'workouts': f'{base_url}/api/workouts/'
     })
 
 class UserViewSet(viewsets.ModelViewSet):
